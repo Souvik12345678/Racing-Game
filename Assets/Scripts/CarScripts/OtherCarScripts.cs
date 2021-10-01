91ca67f44760ca57e71f81e71f8ed2304dbf36ba
Souvik Sardar <35128994+Souvik12345678@users.noreply.github.com>
2021-10-02 00:29:29 +0530

    a

---
+++ Assets/Scripts/CarScripts/OtherCarScripts.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class OtherCarScripts : MonoBehaviour
+{
+    public float minVelocity;
+    public float forceMultipler;
+    Rigidbody2D rBody;
+    private void Awake()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (rBody.velocity.magnitude < minVelocity)
+        {
+            rBody.AddForce(transform.up * forceMultipler);
+        
+        }
+    }
+}
